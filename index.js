1beffe260fc150917453d79fa219c404a250eeba
sidfubo1 <sidfubo1@gmail.com>
2018-07-18 19:10:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 /* Enter the code to remove the main node element under this comment */
+/*var main = document.getElementByID("main")
+main.remove()*/
 
 /* Create your new element here and assign it to newHeader */
-const newHeader = null;
+/*const newHeader = null;*/
